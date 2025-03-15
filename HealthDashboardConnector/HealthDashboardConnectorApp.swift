de02869894829a9980feb55540d2623f23142730
Timeo Williams <timwillie73@gmail.com>
2025-03-15 16:56:59 -0400

    Initial Commit

---
+++ HealthDashboardConnector/HealthDashboardConnectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthDashboardConnectorApp.swift
+//  HealthDashboardConnector
+//
+//  Created by Timeo on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthDashboardConnectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HealthDashboardConnectorTests/HealthDashboardConnectorTests.swift
@@ -0,0 +1,17 @@
+//
+//  HealthDashboardConnectorTests.swift
+//  HealthDashboardConnectorTests
+//
+//  Created by Timeo on 3/15/25.
+//
+
+import Testing
+@testable import HealthDashboardConnector
+
+struct HealthDashboardConnectorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
